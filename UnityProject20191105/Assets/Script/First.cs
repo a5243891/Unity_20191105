1ca5735faed0445b908f914060be88dc7bd59d06
a5243891 <a5243891@yahoo.com.tw>
2019-11-12 21:33:54 +0800

    添加程式

---
+++ UnityProject20191105/Assets/Script/First.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine; // 引用Unity API
+
+// 類別  類別名稱
+public class First : MonoBehaviour
+{
+    // 產出物件
+    // 定義欄位Field(宣告變數)
+    // 修飾詞 欄位類型 欄位名稱 (指定 值)結束
+    // 私人 private - 隱藏(預設)
+    // 公開 public  - 顯示
+    public int level = 1;    // 整數
+    public float speed = 1;  // 浮點數
+    public string prop = "藍色藥水";  // 字串
+    public bool mission = true; // 布林值
+   }
